7db0f70b3fd9771e47b10cf72af2ba2d6201ba8f
ruanjanau <rhuanjanau531@gmail.com>
2023-12-15 19:11:59 -0400

    feat create repository data

---
+++ lib/src/app/data/repository/player_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+
+import '../../domain/entities/players_entity.dart';
+import '../../domain/error/error.dart';
+import '../../domain/repository/repository.dart';
+import '../datasource/datasource.dart';
+
+class PlayersRepository implements IPlayersRepository {
+  final IPlayersDataSource dataSource;
+
+  PlayersRepository(this.dataSource);
+  @override
+  Future<Either<PlayersError, List<PlayersEntity>>> getAll() async {
+    try {
+      var result = await dataSource.getPlayers();
+      return Right(result.map((e) => e.toEntity()).toList());
+    } catch (e) {
+      return Left(PlayersError.unexpected());
+    }
+  }
+}

5749031377bdb0aa993506aa4a8938b30c849ab0
ruanjanau <rhuanjanau531@gmail.com>
2023-12-18 19:02:41 -0400

    alter list player

--- lib/src/app/presentation/view/list_player_page.dart
+++ lib/src/app/presentation/view/list_player_page.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/controller.dart';
+import 'components/details.dart';
 
 class ListPlayerPage extends StatelessWidget {
-  const ListPlayerPage({super.key});
+  final PlayersController _playersController = Get.find<PlayersController>();
+  ListPlayerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lista de Jogadores'),
-      ),
-      body: const Center(child: Text('Olha a Lista aí!!!')),
-    );
+        appBar: AppBar(
+          title: const Text('Lista de Jogadores'),
+        ),
+        body: Center(
+          child: Obx(() {
+            if (_playersController.loading.value) {
+              return const CircularProgressIndicator();
+            } else {
+              return ListView.builder(
+                  itemCount: _playersController.allPlayers.length,
+                  itemBuilder: (_, index) {
+                    final player = _playersController.allPlayers[index];
+                    return PlayersTile(
+                      name: player.name,
+                      profession: player.profession,
+                      image: player.image,
+                    );
+                  });
+            }
+          }),
+        ));
   }
 }

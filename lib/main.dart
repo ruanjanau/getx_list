bc90e33c2a604702b635d2c9ce147a8601d0d632
ruanjanau <rhuanjanau531@gmail.com>
2023-12-18 20:40:45 -0400

    add getmaterial in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:getx_list/src/app/presentation/view/view.dart';
 
 void main() {
@@ -10,13 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ListPlayerPage(),
+      home: const ListPlayerPage(),
     );
   }
 }
